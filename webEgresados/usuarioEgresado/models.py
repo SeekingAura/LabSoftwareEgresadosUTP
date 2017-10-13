e3c328abc2e2441dd04fb771f1051c2f67ad589c
Carlos Moreno <carlox216@gmail.com>
2017-10-13 11:05:41 -0500

    update implemetation and secuential diagram and diagram of activity, state machine
    setup startup diagrams and models of databasament

--- webEgresados/usuarioEgresado/models.py
+++ webEgresados/usuarioEgresado/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UsuariosEgresado(models.Model):
+	user=models.ForeignKey(User)
+	
+	#password=User.password
+	
+	pais=models.CharField(max_length=30)
+	direccionTrabajo=models.CharField(max_length=100)
+	fechaNacimiento=models.DateField()
+	genero=models.CharField(max_length=6)
+	programa=models.CharField(max_length=100)
+	ocupacion=models.CharField(max_length=100)
+	#los intereses serán por tabla pivote
+	
+	#models.IntegerField()
+	#models.FloatField()
