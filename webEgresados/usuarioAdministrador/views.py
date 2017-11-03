35809c328a81251b9671ab3bd4896e4a6dc26fe5
germang2 <germandg99@gmail.com>
2017-11-03 10:39:33 -0500

    templates de index y solicitudes separadas (para admin)

--- webEgresados/usuarioAdministrador/views.py
+++ webEgresados/usuarioAdministrador/views.py
@@ -82,6 +82,4 @@ def index(request):
 				continue
 		print("solo los egresados pendientes", listSoliEgre)
 		context['listSolicitudes']=listSoliEgre
-		testlist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-		context['list'] = testlist
 	return render_to_response('administrador/index.html',context)
